75213afe5ea31776a15f4abdad8a4db97b572614
ngocbq3 <ngocbq@fpt.edu.vn>
2025-02-19 07:39:44 +0700

    change route admin

--- bai3/routes/admin.php
+++ bai3/routes/admin.php
@@ -2,5 +2,6 @@
 
 use App\Controllers\Admin\PostController;
 
-
-$router->get('/admin/posts', [PostController::class, 'index']); // Show all posts
+$router->group(['prefix' => 'admin'], function ($router) {
+    $router->get('/posts', [PostController::class, 'index']); // Show all posts
+});
